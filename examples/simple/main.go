98a3e4ef90572bf4893f9d47897b6a2b1bf1ed4d
Viktor Ogeman <viktor.ogeman@gmail.com>
2012-11-01 20:27:15 +0100

    Addes simple test of calling Lua

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -1,9 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"github.com/vron/lua/core"
 )
 
+var code = `
+io.write("sdfsadf\n");
+x = 0
+for i = 1, #foo do
+	print(i, ,foo[i])
+	x = x + foo[i]
+end
+io.write("kjkjkjkjkjjk\n")
+return x
+`
+
 func main() {
 	core.Test()
+	a := core.Newstate()
+	core.Openlibs(a)
+
+	status := core.Loadstring(a, code)
+	if status != 0 {
+		fmt.Println("Error: ", core.Tostring(a,-1))
+		return
+	}
+	core.Createtable(a,10,10)
+
+	for i:=1.0; i<=5; i++ {
+		core.Pushnumber(a,i)
+		core.Pushnumber(a,i*2)
+		core.Rawset(a,-3)
+	}
+
+	core.Setglobal(a, "foo")
+
+	result := core.Pcall(a,0, core.MULTRET, 0)
+	if (result != 0) {
+		fmt.Println("Error 2")
+		return
+	}
+
+	sum := core.Tonumber(a,-1)
+
+	fmt.Println(sum)
+
+	core.Close(a)
+
+	fmt.Println(a)
 }
